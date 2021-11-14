d517259a5f6c5b9bc996cd8f1dbe08c212624c2f
iAmKevinCruz <kcruzroyalty@gmail.com>
2021-11-14 15:17:01 -0500

    created a Linked List class

--- algo_practice.py
+++ algo_practice.py
@@ -11,13 +11,46 @@ Two strings are only anagrams of each other if all conditions below are met:
     Example: cars and scar, heart and earth, etc.
 """
 
+
 def is_anagram(string_a, string_b):
-    if len(string_a) is not len(string_b):
-        return False
-    # string_a_dict = dict("string_a")
-    test = '{"a": 1, "b": 2}'
-    result = json.loads(test)
-    print(result)
+    new_string_a = string_a.replace(" ", "").lower()
+    new_string_b = string_b.replace(" ", "").lower()
+    if len(new_string_a) is not len(new_string_b):
+        return ("The length is wrong")
+    char_times_a = dict()
+    char_times_b = dict()
+    
+    for i in range(len(new_string_a)):
+        if new_string_a[i] not in char_times_a.keys():
+            char_times_a[new_string_a[i]] = 1
+        else:
+            char_times_a[new_string_a[i]] += 1
+        if new_string_b[i] not in char_times_b.keys():
+            char_times_b[new_string_b[i]] = 1
+        else:
+            char_times_b[new_string_b[i]] += 1
+    
+    return char_times_a == char_times_b
 
 is_anagram("cars", "scar")
 
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items[len(self.items)-1]
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+    def is_empty(self):
+        return self.items == []
