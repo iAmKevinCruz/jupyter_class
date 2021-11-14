5cdd5f41a7a16ff60ab5d6cfcb2bea94d49c241b
iAmKevinCruz <kcruzroyalty@gmail.com>
2021-11-13 23:45:42 -0500

    first commit

---
+++ algo_practice.py
@@ -0,0 +1,19 @@
+"""
+Problem 1
+Given two strings (string_a and string_b) let's check whether or not they are anagrams of each other given the following criteria:
+
+Two strings are only anagrams of each other if all conditions below are met:
+
+    1. They must be of exactly the same length.
+    2. They must use exactly the same characters (no more, no less).
+    Example: cars and scar, heart and earth, etc.
+"""
+
+def is_anagram(string_a, string_b):
+    return string_b
+
+is_anagram("cars", "scar")
+
+for letter in "hello world":
+    print(letter)
+
